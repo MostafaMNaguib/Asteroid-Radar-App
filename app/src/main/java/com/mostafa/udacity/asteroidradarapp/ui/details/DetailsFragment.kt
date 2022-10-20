f412e66d3f58b575fd276e6ce1954270e93e7a78
Mustafa <mostafa.mohamad.naguib@gmail.com>
2022-10-20 03:20:50 +0200

    -- Building Details view screen -- Finalizing app

--- app/src/main/java/com/mostafa/udacity/asteroidradarapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/mostafa/udacity/asteroidradarapp/ui/details/DetailsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.mostafa.udacity.asteroidradarapp.R
 import com.mostafa.udacity.asteroidradarapp.databinding.FragmentDetailBinding
 
 class DetailsFragment: Fragment()
@@ -23,6 +25,23 @@ class DetailsFragment: Fragment()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = this
+
+        val asteroid = DetailsFragmentArgs.fromBundle(requireArguments()).selectedAsteriodDetail
+        binding.asteroid = asteroid
+
+        binding.helpButton.setOnClickListener {
+            displayAstronomicalUnitExplanationDialog()
+        }
+
+    }
+
+    private fun displayAstronomicalUnitExplanationDialog() {
+        val builder = AlertDialog.Builder(requireActivity())
+            .setMessage(getString(R.string.astronomica_unit_explanation))
+            .setPositiveButton(android.R.string.ok, null)
+        builder.create().show()
     }
 
 }
